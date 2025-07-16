b6f691f458facf0bab4c7575a654de10eff48f0d
Manish Kumar <manishhansalmeh@gmail.com>
2025-07-16 20:59:25 +0530

    Fix: Sidebar layout and add onClick functionality on menu icon (#5)

--- pages/components/GitHubNavbar.tsx
+++ pages/components/GitHubNavbar.tsx
@@ -1,9 +1,14 @@
-import { Menu, Search, Plus, Bell, Users, LayoutList, GitMerge, Mail, Sidebar } from 'lucide-react';
+import { Menu, Search, Plus, Bell, Users, LayoutList, GitMerge, Mail } from 'lucide-react';
 import { BTN_PRIMARY_STYLE, BTN_PRIMARY_TAILWIND, IMAGE_CONSTANTS } from '@/utils/constants';
 import ThemeToggleButton from './ThemeToggleButton';
 import { useTheme } from '@/context/ThemeContext';
 
-const GitHubNavbar = () => {
+interface Props {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+}
+
+const GitHubNavbar = ({ open, setOpen }: Props) => {
   const { theme } = useTheme();
   return (
     <nav
@@ -13,7 +18,7 @@ const GitHubNavbar = () => {
     >
       {/* Left Side */}
       <div className="flex items-center gap-3">
-        {/* <button
+        <button
           className={`cursor-pointer rounded-md p-1 hover:bg-[#2f3742] transition-colors flex items-center justify-center ${
             theme === 'dark' ? 'hover:bg-[#21262d]' : 'hover:bg-gray-100'
           }`}
@@ -24,10 +29,11 @@ const GitHubNavbar = () => {
             height: `var(--base-size, 32px)`,
             backgroundColor: theme === 'dark' ? '#161b22' : '#fff',
           }}
+          onClick={() => setOpen(!open)}
         >
           <Menu className="w-5 h-5" style={{ color: `var(--base-icon-black-themed-color)` }} />
-        </button> */}
-        <Sidebar/>
+        </button>
+
         <div className="flex items-center gap-2 text-sm">
           <img
             src={IMAGE_CONSTANTS.profilePicture.src}
